920a6d09b10af2bac8fa7125a96d6affbc1abab6
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-29 07:14:51 +0000

    Code generation

--- components/simple_cta_component_1722235242.js
+++ components/simple_cta_component_1722235242.js
@@ -13,8 +13,11 @@ Vue.component("simple_cta_component_1722235242", {
                     <p id="cta-subtitle" class="flex-1 mb-6 font-light text-gray-600 dark:text-gray-400 md:text-lg">Design and craft stunning fans with ease</p>
                 </div>
                 
-                <div id="cta-button-container" class="flex">
+                <div id="cta-button-container" class="flex flex-wrap justify-center gap-4">
                     <a id="cta-button" href="#" class="flex-1 text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Start Creating</a>
+                    <a href="#" class="flex-1 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Explore Designs</a>
+                    <a href="#" class="flex-1 text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">Join Community</a>
+                    <a href="#" class="flex-1 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Learn More</a>
                 </div>
             </div>
         </div>
